1a29715771f655f60c6791dfb9c493f503c21b10
Sathaphon_Wutthipanyasakul <sathaphon.wutthipanyasakul@gmail.com>
2023-12-31 10:53:53 +0700

    add a new project block-letter called initials

---
+++ initials.cpp
@@ -0,0 +1,34 @@
+// initials.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Project 1: Block Letters
+
+//Instructions: Write a C++ program called initials.cpp that displays the initials of your name in block letters 
+
+#include <iostream>
+
+int main()
+{
+    //funt fucts about me
+    //My sathaporn but most people call me sathaphon
+
+    std::cout << " SSS     A\n";
+    std::cout << "S   S   A A\n";
+    std::cout << "S      A   A\n";
+    std::cout << " SSS   AAAAA\n";
+    std::cout << "    S  A   A\n";
+    std::cout << "S   S  A   A\n";
+    std::cout << " SSS   A   A\n";
+  
+    return 0;
+ 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
